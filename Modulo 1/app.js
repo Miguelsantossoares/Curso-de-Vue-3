f294b70ae2fb32d12b0038b9782ad102d81030dc
Miguel Santos <enium.miguel@gmail.com>
2023-11-17 11:20:54 -0300

    Create app.js

---
+++ Modulo 1/app.js
@@ -0,0 +1,10 @@
+const app = {
+    data () {
+        return {
+            name: 'Carlos',
+            lastName: 'Ferreira - EspecializaTi',
+        }
+    }
+}
+
+Vue.createApp(app).mount('#app')

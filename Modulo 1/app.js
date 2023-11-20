0a6fb1c28e4f771d998bc7630dab1fe3ae688add
Miguel Santos <enium.miguel@gmail.com>
2023-11-20 14:22:37 -0300

    Update app.js

--- Modulo 1/app.js
+++ Modulo 1/app.js
@@ -1,10 +1,65 @@
 const app = {
     data () {
         return {
+            title: 'Curso de Vue 3',
             name: 'Carlos',
             lastName: 'Ferreira - EspecializaTi',
+            products: [
+                {
+                    title: 'Filme 01',
+                    description: 'Filme muito legal',
+                    image: './assets/images/filme01.jpg',
+                    stars: 3,
+                },
+                {
+                    title: 'Filme 02',
+                    description: 'Filme muito legal (2)',
+                    image: './assets/images/filme02.jpg',
+                    stars: 0,
+                },
+            ],
+            cart: [],
+           // textColor: '#000',
+           // bgColor: '#fff'
+           styles: {
+                backgroundColor: '#000',
+                color: '#fff',
+           },
+           themeBlack: true
         }
-    }
+    },
+
+    computed: {
+        fullName () {
+            return this.name + ' ' + this.lastName
+        }
+    },
+
+    methods: {
+        addCart (product) {
+            this.cart.push(product)
+        },
+
+        inCart (product) {
+            return this.cart.indexOf(product) != -1
+        },
+
+        removeCart (product) {
+            this.cart = this.cart.filter((prod, index) => product != prod)
+        },
+
+        toogleTheme () {
+            this.themeBlack = !this.themeBlack
+
+            if (this.themeBlack) {
+                this.styles.backgroundColor = '#000'
+                this.styles.color = '#fff'
+            } else {
+                this.styles.backgroundColor = '#fff'
+                this.styles.color = '#000'
+            }
+        }
+    },
 }
 
 Vue.createApp(app).mount('#app')
